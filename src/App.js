c2e563d2bdfd8590026dbe784f944bd396bb2ba5
Cernan Bernardo <cernanb@gmail.com>
2018-02-13 08:55:18 -0800

    Move fetch action to TaskList

--- src/App.js
+++ src/App.js
@@ -1,70 +1,48 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import Dashboard from './components/Dashboard'
+import Navbar from './components/Navbar'
+import NewTask from './components/NewTask'
+import TaskList from './containers/TaskList'
 
 class App extends Component {
   constructor() {
     super()
-    this.state = {
-      todos: [],
-      newTodo: '',
-    }
   }
 
   componentDidMount() {
-    fetch(`http://localhost:3001/api/v1/tasks`)
-      .then(res => res.json())
-      .then(todos => this.setState({ todos }))
-  }
-
-  handleOnChange(e) {
-    this.setState({
-      newTodo: e.target.value,
-    })
-  }
-
-  addTodo(e) {
-    e.preventDefault()
-    const task = {
-      name: this.state.newTodo,
-    }
-    fetch(`http://localhost:3001/api/v1/tasks`, {
-      method: 'POST',
-      body: JSON.stringify({ task }),
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-    })
-      .then(res => res.json())
-      .then(task =>
-        this.setState({
-          todos: this.state.todos.concat(task),
-          newTodo: '',
-        })
-      )
+    console.log('app mounting')
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React Todo</h1>
-        </header>
-        <h1>My List</h1>
-        <ul>
-          {this.state.todos.map(todo => <li key={todo.id}>{todo.name}</li>)}
-        </ul>
-        <form onSubmit={this.addTodo.bind(this)}>
-          <input
-            type="text"
-            value={this.state.newTodo}
-            onChange={this.handleOnChange.bind(this)}
-          />
-          <button>Add todo</button>
-        </form>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React Todo</h1>
+            <Navbar />
+          </header>
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+            <Route
+              exact
+              path="/tasks"
+              render={props => (
+                <TaskList
+                  {...props}
+                  completeTask={this.completeTask}
+                  something="yes"
+                />
+              )}
+            />
+            <Route path="/tasks/new" component={NewTask} />
+          </Switch>
+        </div>
+      </Router>
     )
   }
 }

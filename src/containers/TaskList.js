10841f543fa50069a3dd41d56257274c9793a8ac
Cernan Bernardo <cernanb@gmail.com>
2018-04-02 16:23:28 -0700

    Add deleteTask and toggleTask actions

--- src/containers/TaskList.js
+++ src/containers/TaskList.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { toggleTodo, deleteTask } from '../redux/actions/taskActions'
 
 import Task from '../components/Task'
 
@@ -6,46 +8,36 @@ class TaskList extends Component {
   state = {
     tasks: [],
   }
-  componentDidMount() {
-    fetch(`http://localhost:3001/api/v1/tasks`)
-      .then(res => res.json())
-      .then(tasks => this.setState({ tasks }))
-  }
 
   toggleTask = id => {
-    const task = this.state.tasks.find(t => t.id === id)
-    const updatedTask = Object.assign({}, task, { completed: !task.completed })
-    fetch(`http://localhost:3001/api/v1/tasks/${id}`, {
-      method: 'PATCH',
-      body: JSON.stringify({ task: updatedTask }),
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-    })
-      .then(res => res.json())
-      .then(updatedTask =>
-        this.setState({
-          tasks: this.state.tasks.map(
-            t => (t.id === updatedTask.id ? updatedTask : t)
-          ),
-        })
-      )
+    const task = this.props.tasks.find(t => t.id === id)
+    this.props.toggleTodo(task)
+  }
+
+  deleteTask = id => {
+    this.props.deleteTask(id)
   }
 
   renderOpenTasks() {
-    return this.state.tasks
+    return this.props.tasks
       .filter(task => !task.completed)
       .map(task => (
-        <Task toggleTask={this.toggleTask} key={task.id} {...task} />
+        <Task
+          postPending={task.postPending}
+          id={task.id}
+          deleteTask={this.deleteTask}
+          toggleTask={this.toggleTask}
+          key={task.id}
+          {...task}
+        />
       ))
   }
 
   renderCompletedTasks() {
-    return this.state.tasks
+    return this.props.tasks
       .filter(task => task.completed)
       .map(task => (
-        <Task toggleTask={this.toggleTask} key={task.id} {...task} />
+        <Task id={task.id} deleteTask={this.deleteTask} toggleTask={this.toggleTask} key={task.id} {...task} />
       ))
   }
 
@@ -61,4 +53,21 @@ class TaskList extends Component {
   }
 }
 
-export default TaskList
+const mapStateToProps = state => {
+  return {
+    tasks: state.tasks,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    toggleTodo(task) {
+      dispatch(toggleTodo(task))
+    },
+    deleteTask(id) {
+      dispatch(deleteTask(id))
+    },
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TaskList)

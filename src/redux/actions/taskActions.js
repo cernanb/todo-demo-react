e80c0046e5ce2e68b6a21b8f20d4aea40980594f
Cernan Bernardo <cernanb@gmail.com>
2018-04-02 16:23:01 -0700

    Build tasks actions

---
+++ src/redux/actions/taskActions.js
@@ -0,0 +1,68 @@
+const uuidv1 = require('uuid/v1')
+
+export const createTask = (task, history) => {
+  return dispatch => {
+    const id = uuidv1()
+    const newTask = Object.assign({}, task, { id, postPending: true })
+    dispatch(addTask(newTask))
+    history.push('/tasks')
+
+    return fetch(`http://localhost:3001/api/v1/tasks`, {
+      method: 'POST',
+      body: JSON.stringify({ task }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(res => res.json())
+      .then(task => {
+        dispatch(createTaskSuccess(id))
+      })
+      .catch(err => dispatch(removeTask(id)))
+  }
+}
+
+const addTask = task => {
+  return { type: 'ADD_TASK', task }
+}
+
+const createTaskSuccess = id => {
+  return {
+    type: 'CREATE_TASK_SUCCESS',
+    id,
+  }
+}
+
+const removeTask = id => {
+  return {
+    type: 'DELETE_TASK_SUCCESS',
+    id,
+  }
+}
+
+export const toggleTodo = todo => {
+  const updatedTask = { ...todo, completed: !todo.completed }
+  return {
+    type: 'TOGGLE_TODO',
+    todo: updatedTask,
+  }
+}
+
+export const getTasks = () => {
+  return async dispatch => {
+    const tasks = await fetch(`http://localhost:3001/api/v1/tasks`).then(res => res.json())
+    dispatch({ type: 'FETCH_TASKS_SUCCESS', tasks })
+  }
+}
+
+export const deleteTask = id => {
+  return dispatch => {
+    return fetch(`http://localhost:3001/api/v1/tasks/${id}`, {
+      method: 'DELETE',
+    })
+      .then(res => res.json())
+      .then(taskId => {
+        dispatch(removeTask(taskId))
+      })
+  }
+}
